e2f5b3bde6012e868796d8d4c94f6665a2e9deee
Jon Hall <jon@hallusa.com>
2019-05-02 08:44:32 -0500

    Initial

--- lbaas.tf
+++ lbaas.tf
@@ -1,6 +1,5 @@
-
 resource "ibm_is_lb" "webtier-lb" {
-  name = "${var.vpc-name}-webtier-lb"
+  name    = "${var.vpc-name}-webtier-lb"
   subnets = ["${ibm_is_subnet.webtier-subnet-zone1.id}", "${ibm_is_subnet.webtier-subnet-zone2.id}"]
 }
 
@@ -9,18 +8,17 @@ resource "ibm_is_lb" "webtier-lb" {
 #  port = "80"
 #  protocol = "http"
 #  default_pool = "${ibm_is_lb_pool.webtier-lb-pool.id}"
-#  connection_limit = "${var.webtier-lb-connections}"
 #}
 
 resource "ibm_is_lb_pool" "webtier-lb-pool" {
-  lb = "${ibm_is_lb.webtier-lb.id}"
-  name = "${var.vpc-name}-webtier-lb-pool"
-  protocol = "http"
-  algorithm = "${var.webtier-lb-algorithm}"
-  health_delay = "5"
-  health_retries = "2"
-  health_timeout = "2"
-  health_type = "http"
+  lb                 = "${ibm_is_lb.webtier-lb.id}"
+  name               = "${var.vpc-name}-webtier-lb-pool"
+  protocol           = "http"
+  algorithm          = "${var.webtier-lb-algorithm}"
+  health_delay       = "5"
+  health_retries     = "2"
+  health_timeout     = "2"
+  health_type        = "http"
   health_monitor_url = "/"
 }
 
@@ -33,6 +31,7 @@ resource "ibm_is_lb_pool" "webtier-lb-pool" {
 #  target_address = "${element(ibm_is_instance.webserver-zone1.*.primary_network_interface.0.primary_ipv4_address,count.index)}"
 #}
 
+
 #resource "ibm_is_lb_pool_member" "webtier-lb-pool-member-zone2" {
 #  count = "${ibm_is_instance.webserver-zone2.count}"
 #  lb    = "${ibm_is_lb.webtier-lb.id}"
@@ -40,3 +39,4 @@ resource "ibm_is_lb_pool" "webtier-lb-pool" {
 #  port  = "80"
 #  target_address = "${element(ibm_is_instance.webserver-zone2.*.primary_network_interface.0.primary_ipv4_address,count.index)}"
 #}
+
