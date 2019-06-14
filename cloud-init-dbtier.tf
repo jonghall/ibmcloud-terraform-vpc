ff7f2a208520a8d0028fa55a56dcb0592fccfeef
Jon Hall <jon@hallusa.com>
2019-06-14 09:16:44 -0500

    Initial

--- cloud-init-dbtier.tf
+++ cloud-init-dbtier.tf
@@ -24,23 +24,21 @@ packages:
 - python3-pip
 - mysql-client
 - mysql-server
+- python3-pymysql
 
 runcmd:
  - echo "deb https://repo.logdna.com stable main" | sudo tee /etc/apt/sources.list.d/logdna.list
  - wget -O- https://repo.logdna.com/logdna.gpg | sudo apt-key add -
  - sudo apt-get update
  - sudo apt-get install logdna-agent < "/dev/null"
- - sudo mysql -u root -Bse "CREATE DATABASE wordpress;"
- - sudo mysql -u root -Bse "CREATE USER wpuser;"
- - sudo mysql -u root -Bse "GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP,ALTER ON wordpress.* TO wpuser@'%' IDENTIFIED BY 'dbpassword';"
- - sudo mysql -u root -Bse "FLUSH PRIVILEGES;"
- - sudo logdna-agent -k logdnakey
+ - sudo logdna-agent -k cfb5a2306a83917968afc44d4691cf6b
  - sudo logdna-agent -s LOGDNA_APIHOST=api.us-south.logging.cloud.ibm.com
  - sudo logdna-agent -s LOGDNA_LOGHOST=logs.us-south.logging.cloud.ibm.com
- - sudo logdna-agent -t webapp-demo
+ - sudo logdna-agent -t databases
  - sudo update-rc.d logdna-agent defaults
  - sudo /etc/init.d/logdna-agent start
- - sudo sed -i "s/^\(bind-address\s*=\s*\).*\$/\10.0.0.0/" mysqld.cnf
+
+ - '\curl -sL https://ibm.biz/install-sysdig-agent | sudo bash -s -- --access_key 94563265-2cea-4c3a-938f-36539f8fa3ee  -c ingest.us-south.monitoring.cloud.ibm.com --collector_port 6443 --secure true -ac "sysdig_capture_enabled: false" --tags role:database'
  - reboot
  EOF
   }
